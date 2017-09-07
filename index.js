f96d4d8d40288f1256ef1dada872735a0c9d4551
mshav <mbulelobakana@gmail.com>
2017-09-07 10:52:51 +0200

    made some changes

--- index.js
+++ index.js
@@ -55,8 +55,11 @@ app.post("/clear", nameRoutes.clear);
 app.post("/clear", nameRoutes.clear);
 
 var namesGreeted = [];
-var port = process.env.PORT || 5000;
-app.listen(port, function(){
-  console.log("app listening on " + "http://localhost:" + port)
 
-})
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
